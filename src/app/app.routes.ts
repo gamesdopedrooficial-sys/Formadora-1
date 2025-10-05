8f698d0fde0138a1b73e4664c150eaf90cb92762
Pedro <gamesdopedrooficial@gmail.com>
2025-10-04 21:28:21 -0300

    Atualização 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './home/home.page';
-import { SobrePage } from './prova/prova.page';
+import { ProvaPage } from './prova/prova.page';
 
 export const routes: Routes = [
   { path: '', component: HomePage },
-  { path: 'sobre', component: SobrePage }
+  { path: 'sobre', component: ProvaPage }
 ];
--- src/app/prova/prova.page.ts
+++ src/app/prova/prova.page.ts
@@ -4,12 +4,23 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-sobre',
+  selector: 'app-prova',  
   templateUrl: './prova.page.html',
   styleUrls: ['./prova.page.scss'],
   standalone: true,
   imports: [IonicModule, CommonModule, RouterModule]
 })
-export class SobrePage {
-  descricao: string = "Olá! Eu sou Pedro Henrique, estudante de Análise e Desenvolvimento de Sistema e sou um estudante que busca se aprimorar e crescer em carreira propria e adquirir um futuro estavel e confortavel para mim e minha familia me estabelencendo de forma profunda e duradoura na area de ti.";
+export class ProvaPage {
+  descricao = "Olá! Eu sou Pedro Henrique, estudante de Análise e Desenvolvimento de Sistema e sou um estudante que busca se aprimorar e crescer em carreira propria e adquirir um futuro estavel e confortavel para mim e minha familia me estabelencendo de forma profunda e duradoura na area de ti.";
+  jokeSetup = "";
+  jokePunchline = "";
+
+  constructor() {}
+
+  async getJoke() {
+    const response = await fetch("https://official-joke-api.appspot.com/random_joke");
+    const data = await response.json();
+    this.jokeSetup = data.setup;
+    this.jokePunchline = data.punchline;
+  }
 }
